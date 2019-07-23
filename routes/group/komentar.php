089fd1ab4a38b011422c23d01ff84f08f3dbba9a
Ihsanp <177006094@student.unsil.ac.id>
2019-07-23 16:20:49 +0700

    ficxdddddds

--- routes/group/komentar.php
+++ routes/group/komentar.php
@@ -1,16 +1,16 @@
 <?php
    // get All Komentar
-   Route::get('/komentars/{id}', "Api\komentarController@getKomentars");
+   Route::get('/komentars/{id}', "Api\KomentarController@getKomentars");
    // Get Reply
    
    Route::middleware(['auth'])->group(function() {
-      Route::get('/reply/{id}', "Api\komentarController@getKomentar");
+      Route::get('/reply/{id}', "Api\KomentarController@getKomentar");
       // Tambah Komentar
-      Route::post("/tambah-komentar","Api\komentarController@tambahKomentar");
+      Route::post("/tambah-komentar","Api\KomentarController@tambahKomentar");
       // Edit Komentar
-      Route::post("/edit-komentar/{id}","Api\komentarController@editKomentar");
+      Route::post("/edit-komentar/{id}","Api\KomentarController@editKomentar");
       // Hapus Komentar
-      Route::get("/hapus-komentar/{id}","Api\komentarController@hapusKomentar");
+      Route::get("/hapus-komentar/{id}","Api\KomentarController@hapusKomentar");
 
    });
    ?>
